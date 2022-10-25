27955ecf58b99714986f9d2f3bb2c7db384f5943
Franco Camposaragna <fcamposaragna@gmail.com>
2022-10-25 12:09:44 -0300

    app terminada

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -11,17 +12,24 @@ export class PorPaisComponent  {
 
   termino:string = '';
   hayError: boolean = false;
+  paises: Country[] = [];
 
   constructor(private paisService:PaisService){}
 
-  buscar(){
+  buscar(termino:string){
     console.log(this.termino);
+    this.termino = termino;
 
     this.paisService.buscarPais(this.termino)
-      .subscribe(resp=>{
-        console.log(resp);
+      .subscribe((paises)=>{
+        console.log(paises);
+        this.paises = paises;
       }, (err)=>{
         this.hayError = true;
+        this.paises = [];
       });
+  }
+  sugerencias(termino:string){
+    this.hayError= false;
   }
 }
